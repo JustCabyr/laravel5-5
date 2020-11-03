035528ece1825ad43311120e9bd522575c32779f
JustCabyr <akasu@dervac.com>
2020-11-03 18:58:08 +0100

    upgrade to Laravel 5.6

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -7,7 +7,7 @@
 	<!--- basic page needs
 	================================================== -->
 	<meta charset="utf-8">
-	<title>{{ isset($post) && $post->seo_title ? $post->seo_title :  __(lcfirst('Title')) }}</title>
+	<title>{{ isset($post) && $post->seo_title ? $post->seo_title :  __(lcfirst('Homestead')) }}</title>
 	<meta name="description" content="{{ isset($post) && $post->meta_description ? $post->meta_description : __('description') }}">
 	<meta name="author" content="@lang(lcfirst ('Author'))">
 	@if(isset($post) && $post->meta_keywords)
